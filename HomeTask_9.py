38c8c6ce92bbacf007244efe4abcc55d1428fa97
leonidych73@gmail.com <Sogtpro73@>
2023-07-06 23:47:47 -0500

    commit_task_9

---
+++ HomeTask_9.py
@@ -0,0 +1,10 @@
+import re
+with open('input.txt', 'r') as inp:
+    data = inp.read()
+    regexp = re.findall(r'[a-zA-Z]', data)
+    with open('output.txt', 'w') as out:
+        for i in regexp:
+            out.write(f'{i} -> pos{regexp.index(i) + 1}\n')
+    with open('output.txt', 'r') as out:
+        output_text = out.read()
+print(output_text)
