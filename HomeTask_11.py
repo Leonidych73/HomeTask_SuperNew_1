9664b405a654011e368ee24ae61048e917348d55
leonidych73@gmail.com <Sogtpro73@>
2023-07-14 00:12:47 -0500

    commit_task_New_11_2

--- HomeTask_11.py
+++ HomeTask_11.py
@@ -9,12 +9,15 @@ Output:  [0, 1, 2]
          [6, 7, 8]
          [9, 10]
 """
-range_values = range(11)
-range_list = list(range_values)
-print(range_list)
+def chunk_generator(range_values, chunk_size):  # create a generator
+    range_list = list(range_values)  # create list
+    for i in range(0, len(range_list), chunk_size):  # iterate over range from 0 to length = len(range_list) with step = chunk_size
+        yield range_list[i:i+chunk_size]  # return a slice equal to the length of the chunk
 
-chunk_size = 3
-for i in range(0, len(range_list), chunk_size):
-   print(range_list[i:i+chunk_size])
-# _______
+my_generator = chunk_generator(range(20), chunk_size=5)  # call the generator
+for chunk in my_generator:  # iterate over each chunk and print it out
+    print(chunk)
+
+
+
 
